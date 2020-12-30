f510d5fc614eef02165ba76b34ab465b667859ec
WaliEEE <jocularriad2@gmail.com>
2020-12-30 14:42:09 +0600

    First Project Setup

---
+++ src/containers/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import HomePage from "../components/homepage.jsx";
+import "./App.scss";
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
